190117c8db403c3813a16804b6dd9e25b59c03d8
Yang Liu <summerliu2333@gmail.com>
2026-02-14 19:48:48 -0800

    Initial commit

---
+++ lab7.cpp
@@ -0,0 +1,14 @@
+//COMSC-210-5068, Lab7, Yang Liu
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+const int ARRAY_SIZE = 5;
+
+string* reverseArray(string* arr, int size);
+void displayArray(string* arr, int size);
+
+int main(){
+return 0;
+}
